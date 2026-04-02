8a132d4cc4b641fa266a8af073cec8c5e71ecfb7
chakerox <chakerox@gmail.com>
2026-04-02 10:53:21 +0700

    membuat fungsi tidak tterus menerus dipanggil

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -36,3 +36,9 @@ void input()
 // create function mergesort
 void mergeSort(int low, int high)
 {
+    if (low >= high) // step 1
+    {
+        return; // step 1.a
+    }
+
+    int mid = (low + high) / 2; // step 2
